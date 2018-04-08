002200465f6eebd3c0a88a39b45af2ad23a676a1
荣洪源 <1506407569@qq.com>
2018-04-08 18:18:14 +0800

    修改用户列表

--- apps/admin/controller/Member.php
+++ apps/admin/controller/Member.php
@@ -21,9 +21,32 @@ class Member extends Base{
             $map['user_type'] = $user_type;
         }
         $list = model('AuthMember')->searchMember($map);
-	    $this->assign('list', $list);
+        $this->assign('list', $list);
+	    $this->assign('count', count($list));
 	    return $this->fetch();
 	}
+
+    /**
+     * 用户启用禁用
+     */
+    public function setStatus(){
+        $type   = input('type');    // 区分状态        1：恢复     2：删除/禁用
+        $method = input('method');  // 区分启禁、删除  1：启用禁用 2：删除
+        $info = '';
+        if($method == 1){
+            $map['status'] = $type;
+            $info = ($type == 2) ? '禁用' : '启用';
+        }else if($method == 2){
+            $map['is_remove'] = $type;
+            $info = ($type == 2) ? '删除用户' : '恢复用户';
+        }
+        $res = model('AuthMember')->setStatus($map, input('ids'));
+        if($res){
+            $this->success($info.'成功');
+        }else{
+            $this->error($info.'失败');
+        }
+    }
 }
 
  ?>
