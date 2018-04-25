20a4bd0baacff5ba15dd39a0f07c7a1c8d421bb8
荣洪源 <1506407569@qq.com>
2018-04-25 11:35:13 +0800

    公共文件增加获取随机字符串

--- apps/common.php
+++ apps/common.php
@@ -98,3 +98,20 @@ function photoPath($data, $type = 1){
     }
     return $imgPath;
 }
+
+/**
+ * [make_password description]
+ * @param  integer $length [description]
+ * @return [type]          [description]
+ */
+function make_random( $length = 8 ){  
+    // 随机数字符集，可任意添加你需要的字符
+    $chars = array('a', 'b', 'c', 'd', 'e', 'f', 'g', 'h','i', 'j', 'k', 'l','m', 'n', 'o', 'p', 'q', 'r', 's','t', 'u', 'v', 'w', 'x', 'y','z', 'A', 'B', 'C', 'D','E', 'F', 'G', 'H', 'I', 'J', 'K', 'L','M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y','Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', '!', '@','#', '$', '%', '^', '&', '*', '(', ')', '-', '_', '[', ']', '{', '}', '<', '>', '~', '`', '+', '=', ',', '.', ';', ':', '/', '?', '|'
+    );
+    $keys = array_rand($chars, $length); // 在 $chars 中随机取 $length 个数组元素键名  
+    $random = '';  
+    for($i = 0; $i < $length; $i++){  
+        $random .= $chars[$keys[$i]];  // 将 $length 个数组元素连接成字符串
+    }  
+    return $random;  
+}  
