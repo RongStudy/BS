478103dbc78f59350fc6fde623652860dca945ab
荣洪源 <1506407569@qq.com>
2018-04-26 16:51:22 +0800

    前台index页面获取商品

--- apps/api/controller/Index.php
+++ apps/api/controller/Index.php
@@ -10,17 +10,30 @@ class Index extends Controller{
     	
         // 获取商品缩略图(每个商品一张)
         foreach ($goods as $key => $value) {
-    		$tempImg[] = explode(',', $value['gImg'])[0];
+    		$tempImgId[] = explode(',', $value['gImg'])[0];
+            $goods[$key]['imgId'] = explode(',', $value['gImg'])[0];
     	}
-    	$tempImg = implode(',', $tempImg);
-    	$goodsImg = model('Attach')->getPhoto(['id'=>['in', $tempImg]]);
-    	$img = photoPath($goodsImg ,1);
+        rsort($tempImgId);
+        $tempImgId2 = $tempImgId;
+        // dump($tempImgId);
 
+    	$tempImgId = implode(',', $tempImgId);
+    	$goodsImg = model('Attach')->getPhoto(['id'=>['in', $tempImgId]]);
+    	$img = photoPath($goodsImg ,1);
+        // dump($img);
+
+        $img2 = array_combine($tempImgId2, $img);
+        foreach ($img2 as $key => $value) {
+            foreach ($goods as $k => $v) {
+                if($v['imgId'] == $key){
+                    $goods[$k]['thumb'] = $value;
+                }
+            }
+        }
     	return json(
             array(
                 'status'=>'1', 
                 'data'=>array(
-                    'img'=>$img,
                     'goods'=>$goods, 
                     'goods_type'=>$goods_type
                 )
